39919a8c0804e8eac726b926dfb56550bb048e07
Kavume <e.gorbacheva404@gmail.com>
2022-12-16 12:28:34 +0100

    feat: add Footer component add Footer, main-styles css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './styles/colors.css'
+import Footer from "./components/Footer";
+import './styles/main-styles.css'
+import './styles/Footer.css';
 
 function App() {
-    return <div>Online-store</div>;
+    return <div className="app-wrapper">
+        <Footer />
+    </div>;
 }
 
 export default App;
