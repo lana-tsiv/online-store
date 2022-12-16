c96a4e141101b1497660d139e0dbd332ba4af5a5
Kavume <e.gorbacheva404@gmail.com>
2022-12-16 16:28:09 +0100

    feat: set routes layout, create pages directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-
+import {Routes, Route} from "react-router-dom";
+import Homepage from './pages/Home-page';
 import './styles/colors.css'
-import Footer from "./components/Footer";
 import './styles/Footer.css';
-import Header from "./components/header";
+import Layout from "./components/Layout";
+import NotFoundPage from "./pages/Not-found-page";
 
-  function App() {
-  return (
-    <div className="app-wrapper">
-      <Header />
-      <Footer />
-    </div>
-  );
+
+function App() {
+    return (
+        <div className="app-wrapper">
+            <Routes>
+                <Route path='/' element={<Layout/>}>
+                    <Route errorElement={<NotFoundPage/>}>
+                        <Route index element={<Homepage/>}/>
+                    </Route>
+                </Route>
+                {/*<Route path='*' element={<NotFoundPage/>}/>*/}
+            </Routes>
+        </div>
+    );
 
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 import './styles/main-styles.css';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import NotFoundPage from "./pages/Not-found-page";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        errorElement: <><NotFoundPage/></>,
+        element: <App/>,
+    },
+]);
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App/>
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
