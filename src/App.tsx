e2d92585c3281ac003d616023edb0ca0c814922f
Lana <tiushkievich00@mail.ru>
2022-12-19 11:03:39 +0100

    feat: update homepage

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,16 @@ import "./styles/colors.css";
 import "./styles/Footer.css";
 import Layout from "./components/Layout";
 import NotFoundPage from "./pages/Not-found-page";
-import Header from "./components/header";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="app-wrapper">
-      <Header />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Homepage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
-      <Footer />
     </div>
   );
 }
