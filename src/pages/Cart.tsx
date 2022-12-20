35156dbeb6d83dc5aff2717fd5d7cff25133140b
Kavume <e.gorbacheva404@gmail.com>
2022-12-20 11:38:55 +0100

    feat: add cart item component

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,7 +1,28 @@
+import {Link} from "react-router-dom";
+import './../styles/Cart.css'
+import {dataCard} from "../data/dataCard";
+import {useState} from "react";
+import CartItem from "../components/CartItem";
+
 const Cart = () => {
+    const scrollToTop = () => {
+            window.scrollTo(0, 0)
+        }
+
+    const [cartItems, setCartItems] = useState(dataCard.filter((item, index) => index < 3));
     return (
-        <div>
-            <h1>Cart page</h1>
+        <div className='cart-container'>
+            <div className='cart-info-wrap'>
+                <h2 className='cart__title'>Shoping cart</h2>
+                <p className='cart__remove-all'>Remove all items</p>
+            </div>
+            {cartItems.map((item) => (
+                <CartItem data={item}/>
+            ))}
+            <div className='cart__buttons'>
+                <Link className='cart-button button-return' to='/' onClick={scrollToTop}>Continue shopping</Link>
+                <button className='cart-button button-buy'>Proceed to checkout</button>
+            </div>
         </div>
     );
 };
