00743e9b3f07c85beeeba324c81abcac969a7d33
Lana <tiushkievich00@mail.ru>
2022-12-20 12:38:56 +0100

    fix: change folder name

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,14 +3,14 @@ import { dataCard } from "../data/dataCard";
 
 const HomePage = () => {
   return (
-      <div className="main-wrapper">
-        <div className="filter">Filter</div>
-        <div className="card">
-          {dataCard.map((item) => (
-            <Cards card={item} />
-          ))}
-        </div>
+    <div className="main-wrapper">
+      <div className="filter">Filter</div>
+      <div className="card">
+        {dataCard.map((item, index) => (
+          <Cards card={item} key={index} />
+        ))}
       </div>
+    </div>
   );
 };
 
