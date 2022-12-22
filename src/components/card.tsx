e0f26fd8ad0cd577f0a0c3ba5820b652858941e1
Lana <tiushkievich00@mail.ru>
2022-12-22 12:51:56 +0100

    fix: card code

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,55 +4,49 @@ import { ICard } from "../models";
 
 import "../styles/card.css";
 
-import {useState} from "react";
-
+import { useState } from "react";
 
 interface CardProps {
-    card: ICard;
-    onClickAddItem: (data: ICard) => void
+  card: ICard;
+  onClickAddItem: (data: ICard) => void;
 }
 
-export function Cards({onClickAddItem, card}: CardProps) {
-    const [stock, setStock] = useState(card.stock)
+export function Cards({ onClickAddItem, card }: CardProps) {
+  const [stock, setStock] = useState(card.stock);
 
-    return (
-        <div className="product-tile">
-            <Link to='/product-card' className="link">
-                <div className='product-tile__info'>
-                    <img
-                        className="product-tile__img"
-                        src={card.photos[0]}
-                        alt={card.productName}
-                    />
-                    <p className='product-tile__brand'>{card.brand}</p>
-                    <p className='product-tile__product-name'>{card.productName}</p>
-                    <p className='product-tile__category'>{card.category}</p>
-                    <div className='price-info-wrap'>
-                        <p className='product-tile__price'>${card.price}</p>
-                        <p className='product-tile__stock'>Stock: {stock}</p>
-                    </div>
-                </div>
-            </Link>
-            <div className="product-tile__buttons">
-                <div className='not-allowed'>
-                    <button className={`button__add-to-cart ${stock === 0 ? 'disabled' : ''}`}
-                            onClick={() => {
-                                if (stock > 0) {
-                                    onClickAddItem(card)
-                                    setStock(stock - 1)
-                                }
-                            }}>Add to cart
-                    </button>
-                </div>
-                <Link to='/product-card'>
-                    <button className='button__details'>Details</button>
-                </Link>
-            </div>
+  return (
+    <div className="product-tile">
+      <Link to="/product-card" className="link">
+        <div className="product-tile__info">
+          <img
+            className="product-tile__img"
+            src={card.photos[0]}
+            alt={card.productName}
+          />
+          <p className="product-tile__brand">{card.brand}</p>
+          <p className="product-tile__product-name">{card.productName}</p>
+          <p className="product-tile__category">{card.category}</p>
+          <div className="price-info-wrap">
+            <p className="product-tile__price">${card.price}</p>
+            <p className="product-tile__stock">Stock: {stock}</p>
+          </div>
         </div>
       </Link>
       <div className="product-tile__buttons">
-        <button className="button__add-to-cart">Add to cart</button>
-        <Link to={`/product-card/${cards.card.id}`}>
+        <div className="not-allowed">
+          <button
+            className={`button__add-to-cart ${stock === 0 ? "disabled" : ""}`}
+            onClick={() => {
+              if (stock > 0) {
+                onClickAddItem(card);
+                setStock(stock - 1);
+              }
+            }}
+          >
+            Add to cart
+          </button>
+        </div>
+        <Link to={`/product-card/${card.id}`}>
           <button className="button__details">Details</button>
         </Link>
       </div>
