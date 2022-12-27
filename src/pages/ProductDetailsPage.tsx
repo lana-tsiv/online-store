21c7aed0d1eb394632d66d5e0743bb2bd4e06c13
Kavume <e.gorbacheva404@gmail.com>
2022-12-27 17:54:47 +0100

    fix: product details styles

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -33,30 +33,36 @@ const ProductDetailsPage = () => {
 
     return (
         <div className='product-details-wrap'>
-            <Breadcrumbs id={currCard.id} title={currCard.brand} category={currCard.category} productName={currCard.productName} />
+            <Breadcrumbs id={currCard.id} title={currCard.brand} category={currCard.category}
+                         productName={currCard.productName}/>
             <div className="product-details">
-                <div className="product-details-image">
+                <div className="product-details__image-wrap">
                     <img
-                        className="product-details-image-photo"
+                        className="product-details__image-photo"
                         src={currCard.photos[0]}
                         alt="view of product"
                     />
                 </div>
                 <div className="product-details__info">
                     <p className="product-details__brand">{currCard.brand}</p>
-                    <p className="product-details__price">${currCard.price}</p>
                     <p className="product-details__product-name">{currCard.productName}</p>
                     <p className="product-details__category">{currCard.category}</p>
                     <p className="product-details__description">{currCard.description}</p>
-                    <button className="product-details-button_add">ADD TO CART</button>
-                    <Popup
-                        trigger={
-                            <button className="product-details-button_buy">BUY NOW</button>
-                        }
-                        position="center center"
-                    >
-                        <Modal/>
-                    </Popup>
+                    <div className='product-details__price-info'>
+                        <p className="product-details__price">${currCard.price}</p>
+                        <p className="product-details__stock">Stock: {currCard.stock}</p>
+                    </div>
+                    <div className='product-details__buttons-wrap'>
+                        <button className="product-details-button_add">ADD TO CART</button>
+                        <Popup
+                            trigger={
+                                <button className="product-details-button_buy">BUY NOW</button>
+                            }
+                            position="center center"
+                        >
+                            <Modal/>
+                        </Popup>
+                    </div>
                 </div>
             </div>
         </div>
