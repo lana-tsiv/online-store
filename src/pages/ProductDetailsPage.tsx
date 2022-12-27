76cde5daec9ae2fe0bfc2cc4785f8f3b6073e38b
Kavume <e.gorbacheva404@gmail.com>
2022-12-27 16:29:39 +0100

    feat: add breadcrumbs component

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -1,62 +1,66 @@
 import Popup from "reactjs-popup";
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {useLocation} from "react-router-dom";
 
-import { ICard } from "../models";
-import { dataCard } from "../data/dataCard";
+import {ICard} from "../models";
+import {dataCard} from "../data/dataCard";
 import Modal from "../components/ModalPopup";
 
 import "./../styles/ProductDetails.css";
+import Breadcrumbs from "../components/Breadcrumbs";
 
 const ProductDetailsPage = () => {
-  const { pathname } = useLocation();
-  const [currCard, setCurrCard] = useState<ICard>({
-    id: 0,
-    productName: "",
-    brand: "",
-    category: "",
-    description: "",
-    price: 0,
-    stock: 0,
-    photos: [],
-  });
-  const currentId = pathname.split("/")[2];
-
-  useEffect(() => {
-    dataCard.forEach((item) => {
-      if (item.id.toString() === currentId.toString()) {
-        setCurrCard(item);
-      }
+    const {pathname} = useLocation();
+    const [currCard, setCurrCard] = useState<ICard>({
+        id: 0,
+        productName: "",
+        brand: "",
+        category: "",
+        description: "",
+        price: 0,
+        stock: 0,
+        photos: [],
     });
-  }, [currentId, setCurrCard]);
+    const currentId = pathname.split("/")[2];
 
-  return (
-    <div className="product-details">
-      <div className="product-details-image">
-        <img
-          className="product-details-image-photo"
-          src={currCard.photos[0]}
-          alt="view of product"
-        />
-      </div>
-      <div className="product-details__info">
-        <p className="product-details__brand">{currCard.brand}</p>
-        <p className="product-details__price">${currCard.price}</p>
-        <p className="product-details__product-name">{currCard.productName}</p>
-        <p className="product-details__category">{currCard.category}</p>
-        <p className="product-details__description">{currCard.description}</p>
-        <button className="product-details-button_add">ADD TO CART</button>
-        <Popup
-          trigger={
-            <button className="product-details-button_buy">BUY NOW</button>
-          }
-          position="center center"
-        >
-          <Modal />
-        </Popup>
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        dataCard.forEach((item) => {
+            if (item.id.toString() === currentId.toString()) {
+                setCurrCard(item);
+            }
+        });
+    }, [currentId, setCurrCard]);
+
+    return (
+        <div className='product-details-wrap'>
+            <Breadcrumbs id={currCard.id} title={currCard.brand} category={currCard.category} productName={currCard.productName} />
+            <div className="product-details">
+                <div className="product-details-image">
+                    <img
+                        className="product-details-image-photo"
+                        src={currCard.photos[0]}
+                        alt="view of product"
+                    />
+                </div>
+                <div className="product-details__info">
+                    <p className="product-details__brand">{currCard.brand}</p>
+                    <p className="product-details__price">${currCard.price}</p>
+                    <p className="product-details__product-name">{currCard.productName}</p>
+                    <p className="product-details__category">{currCard.category}</p>
+                    <p className="product-details__description">{currCard.description}</p>
+                    <button className="product-details-button_add">ADD TO CART</button>
+                    <Popup
+                        trigger={
+                            <button className="product-details-button_buy">BUY NOW</button>
+                        }
+                        position="center center"
+                    >
+                        <Modal/>
+                    </Popup>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ProductDetailsPage;
